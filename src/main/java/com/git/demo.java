644fcabe6f0e37858211479cd762b2af61651304
water <wrd421223@126.com>
2018-09-23 17:17:53 +0800

    123

--- src/main/java/com/git/demo.java
+++ src/main/java/com/git/demo.java
@@ -4,5 +4,10 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("git测试");
         System.out.println("创建分支");
+
+    }
+
+    public static void main1(String[] args) {
+        System.out.println("123");
     }
 }
